29486ed91def834910aab841d03e285829ae6c17
Belen Ramondelli <belen@destinilocators.com>
2023-02-24 10:58:26 -0300

    import fix

--- cypress/support/stepDefinitions/e2e/signInTest.js
+++ cypress/support/stepDefinitions/e2e/signInTest.js
@@ -1,10 +1,10 @@
 import { Given, Then } from '@badeball/cypress-cucumber-preprocessor'
-import SignInPage from '../../pageObjects/signInPage'
+import signInPage from '../../pageObjects/signInPage'
 
 Given('I navigate to Tarmac internal tool', () => {
-    SignInPage.enterURL(globalUrl)
+    signInPage.enterURL(globalUrl)
 })
 
 Then('I should see the google sign in button', () => {
-    SignInPage.getSignInBtn().should('be.visible')
+    signInPage.getSignInBtn().should('be.visible')
 })
